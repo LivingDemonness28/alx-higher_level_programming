2cb03cfa9646386921ad4c67ee1526c335bcd6e1
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-11 01:33:51 +0200

    0x00-python-hello_world: 100-write.py file updated 2nd

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    str = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+    sys.stderr.write(str + '\n')
+    sys.exit(1)
+
+if __name__ == "__main__":
+    main()
