cf951561bf070d670dc07175cfa44c4065882e0f
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-11 00:27:19 +0200

    0x00-python-hello_world: 10-add_nodeint.c file updated 2nd

--- 0x00-python-hello_world/10-add_nodeint.c
+++ 0x00-python-hello_world/10-add_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
-/*
+/**
  * add_nodeint - adds a new node at the beginning of a listint_t list
  * @head: pointer to a pointer of the start of the list
  * @n: integer to be included in node
