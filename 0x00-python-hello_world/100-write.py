fb00ed66950e7f14d703806a840c280ca39b6a40
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-11 01:39:56 +0200

    0x00-python-hello_world: 100-write.py file updated 4th

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 import sys
-
-def main():
-    str = "and that piece of art is useful - Dora Korpar, 2015-10-19"
-    sys.stderr.write(str + '\n')
-    sys.exit(1)
-
-main()
+str = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+sys.stderr.write(str + '\n')
+sys.exit(1)
