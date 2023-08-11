a3a954b6b9ca08b68d759ceb421e4cab28022270
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-11 18:32:38 +0200

    0x01-python-if_else_loops_functions: 2-args.py (executable) file updated 7th

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,14 +4,14 @@ if __name__ == "__main__":
     import sys
     
     argc = len(sys.argv) - 1
-    i = 0
-
     if (argc == 0):
         print("0 arguments.")
     elif (argc == 1):
         print("1 argument:")
     else:
         print("{} arguments:".format(argc))
+    
+    i = 0
     while (i < argc):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
         i = i + 1
