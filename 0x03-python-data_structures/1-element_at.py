4453973231c34ec10c9b6b434dd6fddcdb39a205
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-13 15:03:26 +0200

    0x03-python-data_structure: 1-element_at (executable) file updated 3rd

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    length = len(my_list)
-    if idx < 0 or length <= idx:
+    length = len(my_list) - 1
+    if idx < 0 or length < idx:
         print('None')
     else:
         print("{:d}".format(my_list[idx]))
