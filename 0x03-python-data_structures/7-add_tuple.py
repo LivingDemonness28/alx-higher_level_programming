4058b0d6762c0a4906d008816fabbdd47131a8d5
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-14 17:35:36 +0200

    0x03-python-data_structure: 7-add_tuple.py (executable) file updated 3rd

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,46 +3,13 @@ def add_tuple(tuple_a=(), tuple_b=()):
     list_a = list(tuple_a)
     list_b = list(tuple_b)
 
-    len1 = len(list_a)
-    len2 = len(list_b)
+    while len(list_a) < 2:
+        list_a.append(0)
 
-    if len1 < 2:
-        i = 0
+    while len(list_b) < 2:
+        list_b.append(0)
 
-        while (i < (2-len1)):
-            list_a.append(0)
-            i = i + 1
-    
-    if len2 < 2:
-        j = 0
+    sum_list = [(list_a[0] + list_b[0]), (list_a[1] + list_b[1])]
+    res_tuple = tuple(sum_list)
 
-        while (j < (2-len2)):
-            list_b.append(0)
-            j = j + 1
-    
-    if (2 < len1):
-        list_c = []
-
-        list_c.append(list_a[0])
-        list_c.append(list_a[1])
-
-        list_a = list_c
-    
-    if (2 < len2):
-        list_d = []
-
-        list_d.append(list_b[0])
-        list_d.append(list_b[1])
-
-        list_b = list_d
-    
-    list_e = []
-    k = 0
-
-    while (k < 2):
-        list_e.append((list_a[k] + list_b[k]))
-        k = k + 1
-    
-    tuple_c = tuple(list_e)
-
-    return (tuple_c)
+    return (res_tuple)
