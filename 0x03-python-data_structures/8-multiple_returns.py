aee3f90204fc57429e16bfe196089781f64ef8eb
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-15 00:31:35 +0200

    0x03-python-data_structure: 8-multiple_returns.py (executable) file updated 2nd

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,9 +3,6 @@ def multiple_returns(sentence):
     list1 = []
     length = len(sentence)
     if sentence == "":
-        list1 = [length, 'None']
+        return (len(sentence), 'None')
     else:
-        list1 = [length, sentence[0]]
-    
-    tuple1 = tuple(list1)
-    return (tuple1)
+        return (len(sentence), sentence[0])
