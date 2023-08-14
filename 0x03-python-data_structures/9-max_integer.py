83dd67b56d126797c72f4159fffbed5fc0f30db7
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-15 01:09:05 +0200

    0x03-python-data_structure: 9-max_integer.py file added

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(list) == 0:
+        return ('None')
+    else:
+        my_list = sorted(my_list, reverse=True)
+        print("{:d}".format(my_list[0]))
