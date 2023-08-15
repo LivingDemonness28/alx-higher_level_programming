e082810e9679cf5ac59b72d1d24e558ec6e49496
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-15 02:42:06 +0200

    0x03-python-data_structure: 10-divisible_by_2.py (executable) file updated 3rd

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -7,8 +7,8 @@ def divisible_by_2(my_list=[]):
     while (i < length):
         a = my_list[i] % 2
         if (a == 0):
-            tof_list.append('True')
+            tof_list.append(True)
         elif (a == 1):
-            tof_list.append('False')
+            tof_list.append(False)
         i += 1
     return (tof_list)
