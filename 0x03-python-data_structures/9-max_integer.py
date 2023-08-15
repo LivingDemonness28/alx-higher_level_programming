0b0b80af98e7427014d877a5a54ce22b5cd9e691
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-15 02:08:27 +0200

    0x03-python-data_structure: 9-max_integer.py (executable) file updated 6th

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -8,5 +8,5 @@ def max_integer(my_list=[]):
         while (i < len(my_list)):
             if my_list[i] > max_val:
                 max_val = my_list[i]
+            i = i + 1
         return (max_val)
-
