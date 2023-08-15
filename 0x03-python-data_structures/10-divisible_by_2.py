dac190f3dac2e759268b1192a7f14ab35cd55f21
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-15 02:16:45 +0200

    0x03-python-data_structure: 10-divisible_by_2.py file added

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    length = len(my_list)
+    i  = 0
+    tof_list = []
+
+    while (i < length):
+        a = my_list[i] % 2
+        if (a == 0):
+            tof_list.append('True')
+        else:
+            tof_list.append('False')
+        i += 1
+    return (tof_list)
