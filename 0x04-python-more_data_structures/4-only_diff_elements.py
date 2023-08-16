85e067af8465fbf078177bd1790ad38ef74e1641
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-17 01:09:20 +0200

    0x04-python-more_data_structures: 4-only_diff_elements.py (executable) file updated 2nd

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    common = set_1.intersection(set_2)
-    combined = set_1.union(set_2)
-    not_union = combined.difference(common)
-    return(not_union)
+    return (set_1.union(set_2).difference(set_1.intersection(set_2)))
