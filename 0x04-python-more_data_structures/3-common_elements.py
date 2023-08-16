7b8514032c63439a223b95dbeccc63ca4c6bb261
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-17 00:26:46 +0200

    0x04-python-more_data_structures: 3-common_elements.py (executable) file updated 4th

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    list_1, list_2, list_3 = list(set_1), list(set_2), []
-
-    for i in range(0, len(list_1)):
-        for j in range(0, len(list_2)):
-            if (list_1[i] == list_2[j]):
-                list_3.append(list_2[j])
-    return(set(list_3))
+    common = set_1.intersection(set_2)
+    return (common)
