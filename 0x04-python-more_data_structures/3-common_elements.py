164fba815c17903a200293d253e727ceef804118
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-17 00:00:51 +0200

    0x04-python-more_data_structures: 3-common_elements.py file added

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    list_1 = list(set_1)
+    list_2 = list(set_2)
+    list_3 = []
+
+    for i in range(0, len(list_1)):
+        for j in range(0, len(list_2)):
+            if (list_1[i] == list_2[j]):
+                list_3.append(list_2[j])
+    
+    set_3 = set(list_3)
+    return(set_3)
