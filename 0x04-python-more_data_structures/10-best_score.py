13f6372d68cf238c4c580ead8cc5f26cbf9a4690
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-17 04:26:01 +0200

    0x04-python-more_data_structures: 10-best_score.py (executable) file updated 4th

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary is None or not a_dictionary:
+        return (None)
+    
     max_value = max(a_dictionary.values())
     max_keys = [key for key, value in a_dictionary.items() if value == max_value]
 
     if 1 < len(max_keys) or len(max_keys) <= 0:
         return (None)
-    
-    return (max_keys[0])
+    else:
+        return (max_keys[0])
