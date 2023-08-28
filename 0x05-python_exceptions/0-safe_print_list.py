662f06bdedb492f313686b77e12f85c9d6bd8c41
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-08-28 19:24:44 +0200

    0x05-python_exceptions: 0-safe_print_list.py file added

---
+++ 0x05-python_exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    length = sum(1 for _ in my_list)
+    while x < 0:
+        a = input("Please enter a value for x: ")
+        print(' ')
+        try:
+            x = int(a)
+        except:
+            print("Invalid input, please enter an integer")
+            print(' ')
+            continue
+        if x < 0:
+            print("Number must be 0 or higher")
+            print(' ')
+    res = ''.join(map(str, my_list[0 : x]))
+    print(res)
