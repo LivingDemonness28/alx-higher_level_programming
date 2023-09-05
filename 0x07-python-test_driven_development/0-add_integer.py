988333cd1f7fc0ff0633f337c378060bd382083a
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-09-05 23:58:04 +0200

    0x07-python-test_driven_development: 0-add_integer.py (executable) file updated 3rd

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
+"""Define integer addition function"""
+
+
 def add_integer(a, b=98):
-    if not isinstance(a, (int, float)):
+    """Return int addition of a and b
+    
+    Float arguments typecasted to integer before addition.
+
+    Raises:
+        TypeError: If a or b is neither int nor float.
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    elif not isinstance(b, (int, float)):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+    
+    a = int(a)
+    b = int(b)
+    res = a + b
+    
+    return (res)
