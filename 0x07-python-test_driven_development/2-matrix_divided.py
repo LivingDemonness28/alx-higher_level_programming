946ff55df6528aa772aca1d1086e7c909beaccfd
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-09-06 03:24:11 +0200

    0x07-python-test_driven_development: 2-matrix_divided.py (executable) file updated 2nd

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,27 +8,24 @@ def matrix_divided(matrix, div):
     Float arguments typecasted to integer before addition.
 
     Raises:
-        TypeError: If matrix[i][j] or div is neither int nor float.
+        TypeError: matrix contain a val that isn't an int or float.
+        TypeError: If matrix contains different row sizes.
+        TypeError: If div not an int or float.
         ZeroDivisionError: If div is 0.
     """
+    if (any(len(row) != len(matrix[0]) for row in matrix)):
+        raise TypeError("Each row of the matrix must have the same size")
     if (div == 0):
         raise ZeroDivisionError("division by zero")
-    elif ((not isinstance(div, int) and not isinstance(div, float))):
+    if ((not isinstance(div, int) and not isinstance(div, float))):
         raise TypeError("div must be a number")
-
-    i = 0
-    length1 = len(matrix)
-        
-    while (i < length1):
-        j = 0
-        length2 = len(matrix[i])
-
-        while (j < length2):
+    
+    for i in range(i, len(matrix)):
+        for j in range(j, len(matrix[i])):
             if ((not isinstance(matrix[i][j], int) and not isinstance(matrix[i][j], float))):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-             
+            
             matrix[i][j] = int(matrix[i][j])
             matrix[i][j] = round(matrix[i][j]/div, 2)
-            j = j + 1
-        i = i + 1
+           
     return (matrix)
