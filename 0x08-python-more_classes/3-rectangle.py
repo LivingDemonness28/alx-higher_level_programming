226cd613f105190c29c9355a634e72fd586a0426
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-09-07 18:02:24 +0200

    0x08-python-more_classes: 3-rectangle.py (executable) file updated 5th

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -59,9 +59,9 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ("")
 
-        rect = []
+        rect_list = []
         for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
+            [rect_list.append('#') for j in range(self.__width)]
             if i != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
+                rect_list.append("\n")
+        return ("".join(rect_list))
