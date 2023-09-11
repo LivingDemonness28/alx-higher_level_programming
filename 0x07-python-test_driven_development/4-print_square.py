3d4e78c98189bd0852d18343284c79a968ea95ba
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-09-11 16:57:52 +0200

    0x08-python-more_classes: 4-print_square.py (executable) file updated 2nd

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -13,7 +13,7 @@ def print_square(size):
         ValueError: if size is less than 0
         TypeError: if size is a float and less than 0.
     """
-    if (size < 0):
+    if (isinstance(size, int) and size < 0):
         raise ValueError("size must be >= 0")
     if (isinstance(size, float) and size < 0):
         raise TypeError("size must be an integer")
