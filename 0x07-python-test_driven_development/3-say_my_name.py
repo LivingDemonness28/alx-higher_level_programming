f0bdca2562965bef629cd5e5cf03ff0dda6b616b
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-09-11 02:14:25 +0200

    0x08-python-more_classes: 3-say_my_name.py (executable) file updated 2nd

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -15,5 +15,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if (not isinstance(last_name, str)):
         raise TypeError("last_name must be a string")
-    string = "My name is <{}> <{}>".format(first_name, last_name)
-    return (string)
+    print("My name is <{}> <{}>".format(first_name, last_name))
