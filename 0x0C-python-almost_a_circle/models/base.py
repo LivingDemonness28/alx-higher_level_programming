b295b8bb0e3e08393a759b9e4a05e76c7c440681
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-09-19 17:57:53 +0200

    0x0C-python_almost_a_circle/models: base.py file added

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a base model class."""
+
+
+class Base:
+    """Represents a Base model for all
+    other classes in the project.
+
+    Private Class Attributes:
+        __nb_object (int): Num of instantiated bases.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initializes a new Base.
+
+        Args:
+            id (int): Identity of new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
