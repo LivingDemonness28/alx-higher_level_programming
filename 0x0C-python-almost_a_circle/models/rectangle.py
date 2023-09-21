67224e583f09a3517d42f14dc705b496e221cf6c
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-09-21 23:01:10 +0200

    0x0C-python_almost_a_circle/models: rectangle.py file updated 13th  - task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,5 +91,5 @@ class Rectangle(Base):
     def __str__(self):
         """Returms formatted string representation of
         Rectangle instance."""
-        return (f"[Rectangle] ({self.id}) {self.x}/{self.y} -" +
+        return (f"[Rectangle] ({self.id}) {self.x}/{self.y} - " +
                 f"{self.width}/{self.height}")
