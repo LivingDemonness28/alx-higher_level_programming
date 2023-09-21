135b98b04f1e24a5c019e0e80b5ae55b71586bde
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-09-22 01:36:15 +0200

    0x0C-python_almost_a_circle/models: square.py file added  - task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initialize a Square instance.
+
+        Args:
+        - size (int): Size of square.
+        - x (int): x-coordinate of square's position (default is 0).
+        - y (int): y-coordinate of square's position (default is 0).
+        - id (int): ID of the square (default is None).
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Return a formatted string representation
+        of the Square instance.
+        """
+        return (f"[Square] ({self.id}) {self.x}/{self.y}" +
+                f"- {self.width}")
