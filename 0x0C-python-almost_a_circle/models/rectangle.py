3a8e35b6f15fa8c3f95bf59cd211ed2386ed5ff7
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-09-22 02:03:43 +0200

    0x0C-python_almost_a_circle/models: rectangle.py file updated 20th  - task 13

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -139,3 +139,16 @@ class Rectangle(Base):
                     self.x = v
                 elif (k == "y"):
                     self.y = v
+
+    def to_dictionary(self):
+        """Return dictionary representation
+        of Rectangle."""
+        return (
+            {
+                "id": self.id,
+                'width': self.width,
+                "height": self.height,
+                "x": self.x,
+                "y": self.y
+            }
+        )
