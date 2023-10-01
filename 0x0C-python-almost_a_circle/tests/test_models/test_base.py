c87da5d2dcc71e0532c427937fa3c42f7c919c05
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-10-02 01:55:39 +0200

    0x0C-python-almost_a_circle/test/test_models: test_base.py file (executable) updated 3rd

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,7 +2,7 @@
 """Defines unittests for base.py.
 
 Unittest classes:
-    TestBase - line 21
+    TestBase_instantiate - line 12
 """
 import unittest
 from models.base import Base
@@ -11,12 +11,12 @@ from models.square import Square
 
 class TestBase_instantiate(unittest.TestCase):
     """Unittests for instantiation of the Base class."""
-    def test_auto_increment_id(self):
+    def test_three_bases(self):
         base1 = Base()
         base2 = Base()
         base3 = Base()
 
-        +self.assertEqual(base1.id, 1)
+        self.assertEqual(base1.id, 1)
         self.assertEqual(base2.id, 2)
         self.assertEqual(base3.id, 3)
 
