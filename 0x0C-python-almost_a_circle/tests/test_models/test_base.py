1219d28d54d668f7b5586d546bd42db252e67f6f
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-10-02 02:00:03 +0200

    0x0C-python-almost_a_circle/test/test_models: test_base.py file (executable) updated 4th

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -11,14 +11,10 @@ from models.square import Square
 
 class TestBase_instantiate(unittest.TestCase):
     """Unittests for instantiation of the Base class."""
-    def test_three_bases(self):
+    def test_two_bases(self):
         base1 = Base()
         base2 = Base()
-        base3 = Base()
-
-        self.assertEqual(base1.id, 1)
-        self.assertEqual(base2.id, 2)
-        self.assertEqual(base3.id, 3)
+        self.assertEqual(base1.id, base2.id - 1)
 
 if __name__ == '__main__':
     unittest.main()
