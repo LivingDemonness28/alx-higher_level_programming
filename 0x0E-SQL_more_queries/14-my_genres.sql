50e70a21ca88f1aa40d30fc77e4ff336750ad4b9
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-12-14 00:40:04 +0200

    0x0E-SQL_more_queries: 14-my_genres.sql file updated 2nd

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
--- Write a script that lists all Comedy
--- shows in the database hbtn_0d_tvshows.
+-- Write a script that uses the hbtn_0d_tvshows
+-- database to lists all genres of the show Dexter.
 SELECT name
 FROM tv_genres
 LEFT JOIN tv_show_genres
