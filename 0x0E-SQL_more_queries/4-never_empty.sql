2afd612d80f07594d702d3945216017a43d8aa4a
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-12-13 23:41:21 +0200

    0x0E-SQL_more_queries: 4-never_empty.sql file updated 2nd

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- Write a script that creates the table
 -- id_not_null on your MySQL server.
 SET @dbName = 'your_database';
-CREATE TABLE IF NOT EXISTS `your_database`.`id_not_null` (`id` INT DEFAULT 1, `name` VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
 SELECT 'Table id_not_null created or already exists' AS result;
