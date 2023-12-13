8720998f75d9ef8b1a6df81a1d3b87b72ace7be0
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-12-13 15:12:43 +0200

    0x0E-SQL_more_queries: 3-force_name.sql file updated 3rd

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -2,4 +2,3 @@
 -- force_name on your MySQL server.
 SET @dbName = 'your_database';
 CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
-SELECT 'Table force_name created or already exists' AS result;
