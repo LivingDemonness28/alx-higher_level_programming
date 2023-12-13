8f96d72bf50e46047c1b874fe397785d8e320c38
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-12-13 15:04:52 +0200

    0x0E-SQL_more_queries: 3-force_name.sql file added

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- Write a script that creates the table
+-- force_name on your MySQL server.
+SET @dbName = 'your_database';
+CREATE TABLE IF NOT EXISTS `your_database`.`force_name` (`id` INT, `name` VARCHAR(256) NOT NULL);
+SELECT 'Table force_name created or already exists' AS result;
