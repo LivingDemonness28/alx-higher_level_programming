cda5219ab689d69e4f5730738c738beb2c0abc3b
LivingDemonness28 <ajamshamoodah@gmail.com>
2024-01-08 14:43:15 +0200

    0x12-javascript-warm_up: squares.js file added

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const string = process.argv[2];
+const number = parseInt(string);
+
+if (isNaN(number)) {
+  console.log('Missing size');
+} else {
+  let string1 = 'X';
+
+  for (let i = 0; i < number; i++) {
+    string1 += string1;
+  }
+
+  for (let j = 0; j < number; j++) {
+    console.log(string1);
+  }
+}
+
+
