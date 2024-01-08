2446541c12cf986240a04cf397fc9b1eb2146f03
LivingDemonness28 <ajamshamoodah@gmail.com>
2024-01-08 12:39:54 +0200

    0x12-javascript-warm_up: 2-arguments.js (exe) file updated 3rd

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const argument = process.argv.length - 2;
-if (argument === 0){
+if (argument === 0) {
   console.log('No argument');
 } else if (argument === 1) {
   console.log('Argument found');
