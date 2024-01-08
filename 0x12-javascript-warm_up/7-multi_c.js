33bad2771b626162ec14a6c3e7f31b409de26ae6
LivingDemonness28 <ajamshamoodah@gmail.com>
2024-01-08 14:10:05 +0200

    0x12-javascript-warm_up: 7-multi_c.js (exe) file updated 4th

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ const number = parseInt(string);
 if (isNaN(number)) {
   console.log('Missing number of occurences');
 } else {
-  for (let i = 1; i <= number; i++) {
+  for (let i = 0; i <= number; i++) {
     console.log('C is fun');
   }
 }
