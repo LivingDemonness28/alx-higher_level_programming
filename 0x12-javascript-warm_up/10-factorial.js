7ccfb2fea4577e5b0aadb43ef35795f636c8ddbe
LivingDemonness28 <ajamshamoodah@gmail.com>
2024-01-08 21:16:28 +0200

    0x12-javascript-warm_up: 10-factorial.js (exe) file updated 8th

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,9 +3,7 @@ const number = process.argv[2];
 const n = parseInt(number);
 
 function fact (n) {
-  if (number === undefined) {
-    console.log(1);
-  } else if (isNaN(n)) {
+  if (n === undefined || isNaN(n)) {
     console.log(1);
   } else {
     return (n * fact(n - 1));
