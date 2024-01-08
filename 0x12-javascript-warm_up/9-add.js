9754ffd768de69c63733a8363474d308224ab30b
LivingDemonness28 <ajamshamoodah@gmail.com>
2024-01-08 19:38:36 +0200

    0x12-javascript-warm_up: 9-add.js file added

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const string1 = process.argv[2];
+const string2 = process.argv[3];
+
+const a = parseInt(string1);
+const b = parseInt(string2);
+
+function add(a, b) {
+  console.log(a + b);
+}
