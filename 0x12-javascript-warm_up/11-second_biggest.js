4ada3db6e2e54460cf10a45ba89ab343b5e5ee51
LivingDemonness28 <ajamshamoodah@gmail.com>
2024-01-09 02:09:09 +0200

    0x12-javascript-warm_up: 11-second_biggest.js (exe) file updated 5th

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,10 +3,8 @@ const arr1 = process.argv.slice(2);
 
 function second_biggest (arr1) {
   len1 = arr1.length
-  if (arr1 === undefined) {
-    return (0);
-  } else if (len1 === 1) {
-    return (0);
+  if (arr1 === undefined || len1 <= 1) {
+    console.log(0);
   } else {
     while (true) {
       let swapped = false;
@@ -22,8 +20,8 @@ function second_biggest (arr1) {
         break;
       }
     }
-    return (arr1[len1 - 2]);
+    console.log(arr1[len1 - 2]);
   }
 }
 
-console.log(second_biggest(arr1));
+second_biggest(arr1);
